d17cb89b544eb66cc3b7305aa30012db09c8aee0
lizardman <34143642+lizardman97@users.noreply.github.com>
2019-05-09 22:06:55 +0200

    visuel

--- content/js/app.js
+++ content/js/app.js
@@ -1,37 +1,24 @@
 var population = [];
+var populationTrier = [];
+var nbGeneration = 0;
 
 function btGenerate(){
+    clear();
     generationPopulation();
-    testAff();
+    affichagePopulation();
+    nbGeneration += 1;
+    majTitre();
 }
 
 function generationPopulation(){
     var i = 0;
-    var nbGenome = 8;
+    var nbGenome = document.getElementById("nbGenome").value;
     do {
         var gene = getIndividu(2);
         population[i] = gene;
         i += 1;
     } while(i < nbGenome)
 }
-
-
-function plus(){
-    var nbActuel = document.getElementById("nbGene").innerHTML;
-    var nbSuivant = nbActuel * 2;
-    var moyen = nbActuel;
-    document.getElementById("nbGene").innerHTML = nbSuivant;
-    document.getElementById("GeneMoyen").innerHTML = moyen;
-}
-
-function moins(){
-    var nbActuel = document.getElementById("nbGene").innerHTML;
-    var nbSuivant = nbActuel / 2;
-    var moyen = nbSuivant / 2;
-    document.getElementById("nbGene").innerHTML = nbSuivant;
-    document.getElementById("GeneMoyen").innerHTML = moyen;
-}
-
 
 function score(ix){
     var i = 0;
@@ -48,38 +35,62 @@ function score(ix){
     return score;
 }
 
-
-
 function getIndividu(max) {
 
     var individu = [];
     var i = 0;
     var gene;
     var nb = document.getElementById("nbGene").innerHTML;
-
     do {
         gene =  Math.floor(Math.random() * Math.floor(max));
         individu[i] = gene ;
         i += 1
     } while(i<nb)
-
-    // return console.log("individu : " + individu);
     return individu;
 }
 
-
-
-function testAff(){
+function affichagePopulation(){
     var i = 0;
     do {
         document.getElementById("affPop").innerHTML += "<br><br> individu "+ (i + 1) +" : " + population[i] + "<br> score  : " + score(population[i]) ;
         i += 1;
-    } while(i < population.length)
+    } while(i < document.getElementById("nbGenome").value)
 }
 
 function clear(){
     document.getElementById("affPop").innerHTML="";
 }
+
+function majTitre(){
+    var titre = "genetic algorith - generation n°"
+    document.getElementById("titre").innerHTML = titre + nbGeneration;
+}
+
+function btTri(){
+    
+    var i = 0;
+    var max = 0;
+    var temp = 0;
+    var point = []
+
+    do {
+        point[i] = score(population[i]);
+        
+        i += 1;
+    } while(i < document.getElementById("nbGenome").value);
+    console.log("best : " + Math.max(point));
+    console.log(point);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -87,4 +98,23 @@ function reset(){
     document.getElementById("nbGene").innerHTML = 8;
     document.getElementById("GeneMoyen").innerHTML = 4;
     document.getElementById("affPop").innerHTML = "";
+    document.getElementById("nbGenome").value = 8;
+    document.getElementById("titre").innerHTML = "genetic algorith";
+    nbGeneration = 0;
+}
+
+function plus(){
+    var nbActuel = document.getElementById("nbGene").innerHTML;
+    var nbSuivant = nbActuel * 2;
+    var moyen = nbActuel;
+    document.getElementById("nbGene").innerHTML = nbSuivant;
+    document.getElementById("GeneMoyen").innerHTML = moyen;
+}
+
+function moins(){
+    var nbActuel = document.getElementById("nbGene").innerHTML;
+    var nbSuivant = nbActuel / 2;
+    var moyen = nbSuivant / 2;
+    document.getElementById("nbGene").innerHTML = nbSuivant;
+    document.getElementById("GeneMoyen").innerHTML = moyen;
 }
--- index.php
+++ index.php
@@ -9,48 +9,30 @@
     <title>genetic algorithm</title>
 </head>
 <body>
-    <h2>genetic algorithm</h2>
+    <h2 id="titre">genetic algorithm</h2>
 
     <div class="forJS">
     
-        <!-- <button id="btGenerate" onclick="getRandomInt(2)">Generate</button> -->
+        <label for="nbGenome">Nombre d'individu de la population</label>
+        <input type="number" name="nbGenome" id="nbGenome" value="8" max="500" min="8" step="1">
+        <br>
         <button id="geneUP" onclick='plus()'>+</button>
         <button id="geneDOWN" onclick='moins()'>-</button>
         <p id="test">Nombre de genes de la population : <p id="nbGene">8</p></p>
         <p>moyenne : <p id="GeneMoyen">4</p></p>
         <button id="btGenerate" onclick="btGenerate()">Generate</button>
-        <!-- <button id="btGenerate" onclick="affPopulation()">Generate</button> -->
-        <!-- <button id="btGenerate" onclick="testAff()">Generate</button> -->
-        <button id="btSort" onclick="affTri()">Sort</button>
+        <button id="btSort" onclick="btTri()">Sort</button>
         <button id="btMuatate">Mutate</button>
         <button id="btFill">Fill</button>
         <button id="btReset" onclick="reset()">reset</button>
 
         <br>        <br>        <br>
-    
-        <div class="population1">
-            <p id="pop1"></p><p id="score1"></p>
-            <p id="pop2"></p><p id="score2"></p>
-            <p id="pop3"></p><p id="score3"></p>
-            <p id="pop4"></p><p id="score4"></p>
-            <p id="pop5"></p><p id="score5"></p>
-            <p id="pop6"></p><p id="score6"></p>
-            <p id="pop7"></p><p id="score7"></p>
-            <p id="pop8"></p><p id="score8"></p>
-        </div>
 
-        <div class="population1Trier">
-            <p id="popT1"></p>
-            <p id="popT2"></p>
-            <p id="popT3"></p>
-            <p id="popT4"></p>
-            <p id="popT5"></p>
-            <p id="popT6"></p>
-            <p id="popT7"></p>
-            <p id="popT8"></p>
-        </div>
-
+        <label for="affPop">non trier</label>
         <p id="affPop"></p>
+
+        <label for="affPopTrier">trier</label>
+        <p id="affPopTrier"></p>
 
     </div>
 </body>
